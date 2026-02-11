e2871d1f89b75d38c4d0d3d16f8553e19acebc05
Anonymity <86890576+mushdreamer@users.noreply.github.com>
2026-02-10 19:36:20 -0500

    UI调整

--- Assets/SpaceFusion/SF Grid Building System/Scripts/UI/PlaceableActionTooltip.cs
+++ Assets/SpaceFusion/SF Grid Building System/Scripts/UI/PlaceableActionTooltip.cs
@@ -9,22 +9,7 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.UI
     public class PlaceableActionTooltip : MonoBehaviour
     {
         [SerializeField]
-        private Button moveButton;
-
-        [SerializeField]
         private Button removeButton;
-
-        [Header("Institute Buttons")]
-        [SerializeField]
-        private Button researchButton;
-
-        // <<< +++ 
-        // +++ 1. 新增: 添加对 "升级" 按钮的引用
-        // +++ 
-        [Header("Building Action Buttons")]
-        [SerializeField]
-        private Button upgradeButton;
-        // <<< +++ -------------------------- +++
 
         [SerializeField]
         private GameObject tooltipUI;
@@ -52,17 +37,12 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.UI
             {
                 _instance = this;
                 _tooltipSize = GetComponent<RectTransform>().sizeDelta;
-                moveButton.onClick.AddListener(MoveObject);
-                removeButton.onClick.AddListener(RemoveObject);
 
-                // <<< +++ 
-                // +++ 2. 新增: 绑定新 "升级" 按钮的点击事件
-                // +++ 
-/*                if (upgradeButton != null) // (添加一个非空检查更安全)
+                // 仅保留删除按钮的监听
+                if (removeButton != null)
                 {
-                    upgradeButton.onClick.AddListener(UpgradeObject);
-                }*/
-                // <<< +++ ---------------------------------- +++
+                    removeButton.onClick.AddListener(RemoveObject);
+                }
 
                 tooltipUI.gameObject.SetActive(false);
             }
@@ -72,13 +52,17 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.UI
         {
             _placementSystem = PlacementSystem.Instance;
             _targetCamera = GameManager.Instance.SceneCamera;
+
+            // 绑定退出/关闭逻辑
             InputManager.Instance.OnExit += CloseTooltip;
             PlacedObject.holdComplete += ShowTooltip;
         }
 
         private void OnDestroy()
         {
-            InputManager.Instance.OnExit -= CloseTooltip;
+            if (InputManager.Instance != null)
+                InputManager.Instance.OnExit -= CloseTooltip;
+
             PlacedObject.holdComplete -= ShowTooltip;
         }
 
@@ -92,38 +76,8 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.UI
         {
             _placedObject = caller;
 
-            // --- 修复逻辑：处理可能的组件缺失 ---
-            BuildingType buildingType;
-
-            // 检查是否是普通建筑
-            if (_placedObject.buildingEffect != null)
-            {
-                buildingType = _placedObject.buildingEffect.type;
-            }
-            else
-            {
-                // 如果是教学建筑（没有 BuildingEffect），我们给它一个默认行为
-                // 或者你可以根据 TutorialBuildingEffect 的类型来决定显示什么
-                Debug.Log("[Tooltip] 检测到教学建筑，使用通用 UI 逻辑。");
-
-                // 这里暂时设为 House 类型以显示基础的 移动/拆除 按钮
-                buildingType = BuildingType.House;
-            }
-
-            moveButton.gameObject.SetActive(true);
+            // 仅显示删除按钮，不再根据建筑类型切换其他按钮
             removeButton.gameObject.SetActive(true);
-
-            // 按钮显示逻辑
-            if (buildingType == BuildingType.Institute)
-            {
-                researchButton.gameObject.SetActive(true);
-                if (upgradeButton != null) upgradeButton.gameObject.SetActive(false);
-            }
-            else
-            {
-                researchButton.gameObject.SetActive(false);
-                if (upgradeButton != null) upgradeButton.gameObject.SetActive(true);
-            }
 
             var screenPosition = _targetCamera.WorldToScreenPoint(caller.transform.position);
             tooltipUI.gameObject.SetActive(true);
@@ -155,15 +109,12 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.UI
             return newPosition;
         }
 
-        private void MoveObject()
-        {
-            _placementSystem.StartMoving(_placedObject);
-            HideTooltip();
-        }
-
         private void RemoveObject()
         {
-            _placementSystem.Remove(_placedObject);
+            if (_placedObject != null)
+            {
+                _placementSystem.Remove(_placedObject);
+            }
             HideTooltip();
         }
 
@@ -173,29 +124,12 @@ namespace SpaceFusion.SF_Grid_Building_System.Scripts.UI
             blockerUI.SetActive(false);
         }
 
-/*        private void FundResearch()
-        {
-            ResourceManager.Instance.FundResearch();
-            HideTooltip();
-        }*/
-
-        // <<< +++ 
-        // +++ 4. 新增: 升级按钮调用的方法
-        // +++ 
-/*        private void UpgradeObject()
-        {
-            // 它会调用我们之前在 BuildingEffect.cs 中创建的 TryUpgradeBuilding 方法
-            if (_placedObject != null && _placedObject.buildingEffect != null)
-            {
-                _placedObject.buildingEffect.TryUpgradeBuilding();
-            }
-            HideTooltip(); // 无论升级是否成功 (比如钱不够)，都关闭菜单
-        }*/
-        // <<< +++ ---------------------------------- +++
-
         private static void ShowTooltip(PlacedObject caller)
         {
-            _instance.Show(caller);
+            if (_instance != null)
+            {
+                _instance.Show(caller);
+            }
         }
     }
 }
